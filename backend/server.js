c06f5d97365a8642cfd310a0435cf19f5925531d
Jayandra Hamal <salonhamal278@gmail.com>
2024-01-12 22:50:45 +0530

    router in express

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const router = require('./router/auth-router')
 
 const PORT = 5000;
 
-app.get('/', (req,res)=> {
-    res.status(200).send("welcome");
-})
+app.use('/api/auth', router)
+
+
 
 app.listen(PORT, () => {
     console.log(`server is running at port: ${PORT}`)
