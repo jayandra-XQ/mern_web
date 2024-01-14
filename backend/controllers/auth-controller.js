04a90d43656a709620ac3a6654484172b071bc69
Jayandra Hamal <salonhamal278@gmail.com>
2024-01-14 10:20:50 +0530

    contact route

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -39,4 +39,33 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = { home, register }
+// login logic
+
+const login = async (req,res) => {
+    try {
+        const {email, password} = req.body
+
+        const userExit = await User.findOne({email})
+
+        if(!userExit) {
+            return res.status(400).json({ message: "Invalid Credentials"});
+        }
+
+        const user = await bcrypt.compare(password, userExit.password)
+
+        if(user) {
+            res.status(200).json({
+                msg: "Login successful",
+                token: await userExit.generateToken(),
+                userId: userExit._id.toString()
+            })
+        } else {
+            res.status(401).json({msg: "Invalid email or password"})
+        }
+
+    } catch (error) {
+        res.status(500).json('internal server error')
+    }
+}
+
+module.exports = { home, register, login }
--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const authControllers = require('../controllers/auth-controller')
+const validate = require("../middlewares/validate-middleware")
+const signupSchema = require("../validators/auth-validator")
 
 
 router.get('/', authControllers.home)
-router.post('/register', authControllers.register)
+router.post('/register', validate(signupSchema), authControllers.register)
+
+
+router.post('/login', authControllers.login)
 
 
 module.exports = router
